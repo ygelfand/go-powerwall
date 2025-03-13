c76d6ff89bdaf78e7f3db5aadbb74b3b03c34442
ygelfand <yuri@shlitz.com>
2025-03-13 12:42:51 -0400

    fix fans for real
    Signed-off-by: ygelfand <yuri@shlitz.com>

--- internal/api/fans.go
+++ internal/api/fans.go
@@ -15,12 +15,27 @@ func (app *Api) parsedFans() map[string]PvacFan {
 	if app.powerwall.Controller == nil {
 		return fans
 	}
-	for id, inv := range app.powerwall.Controller.EsCan.Bus.Pvac {
-		if !inv.PVACStatus.IsMIA {
-			idx := pvacIndex(id)
-			fans[fmt.Sprintf("A%s", idx)] = PvacFan{
-				ActualRpm: inv.PVACLogging.PVAC_Fan_Speed_Actual_RPM,
-				TargetRpm: inv.PVACLogging.PVAC_Fan_Speed_Target_RPM,
+	var indexActual, indexTarget int
+	for _, msa := range app.powerwall.Controller.Components.Msa {
+		for _, signal := range msa.Signals {
+			if signal.Name == "PVAC_Fan_Speed_Actual_RPM" && signal.Value != nil {
+				key := fmt.Sprintf("A%s", pvacIndex(indexActual))
+				fan, exists := fans[key]
+				if !exists {
+					fan = PvacFan{} // Initialize if not present
+				}
+				fan.ActualRpm = int(*signal.Value)
+				fans[key] = fan
+				indexActual++
+			} else if signal.Name == "PVAC_Fan_Speed_Target_RPM" && signal.Value != nil {
+				key := fmt.Sprintf("A%s", pvacIndex(indexTarget))
+				fan, exists := fans[key]
+				if !exists {
+					fan = PvacFan{} // Initialize if not present
+				}
+				fan.TargetRpm = int(*signal.Value)
+				fans[key] = fan
+				indexTarget++
 			}
 		}
 	}
--- internal/api/temps.go
+++ internal/api/temps.go
@@ -8,14 +8,15 @@ import (
 
 func (app *Api) temps(c *gin.Context) {
 	temps := map[string]*float32{}
-	index := 0
-	for _, msa := range app.powerwall.Controller.Components.Msa {
-		for _, signal := range msa.Signals {
-			if signal.Name == "THC_AmbientTemp" && signal.Value != nil {
-				index++
-				temps[fmt.Sprintf("PW%v_temp", index)] = signal.Value
+	if app.powerwall.Controller != nil {
+		index := 0
+		for _, msa := range app.powerwall.Controller.Components.Msa {
+			for _, signal := range msa.Signals {
+				if signal.Name == "THC_AmbientTemp" && signal.Value != nil {
+					index++
+					temps[fmt.Sprintf("PW%v_temp", index)] = signal.Value
+				}
 			}
-
 		}
 	}
 	c.JSON(200, temps)
